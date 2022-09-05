8cc133bb475389f69b41e90d2667dd4487d58c6a
Kamran <83241419+NarmakTwo@users.noreply.github.com>
2022-09-05 09:38:52 -0400

    added cla

---
+++ maincmd.py
@@ -0,0 +1,21 @@
+import crack
+import sys
+from datetime import date
+from os import name, system
+
+def clear():
+  if name == 'nt':
+    system('cls')
+
+  else:
+    system('clear')
+
+
+aSettings = {
+  "mLength":sys.argv[2]
+}
+
+pwd, id = crack.crack(sys.argv[1], aSettings, clear)
+
+with open('passwords.csv','a') as f:
+    f.write(f'\n{date.today().strftime("%d/%m/%Y")}, {id}, {pwd}')
